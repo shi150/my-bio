cafade1a0764259c7b4972a57b1670b52c4d11aa
shivani123 <shivaninagelli@gmail.com>
2024-12-06 15:44:52 +0530

    Installed Libraries

---
+++ subract.py
@@ -0,0 +1,6 @@
+print("Hello , I am going to perform subraction")
+
+num = int(input("Enter your first integer"))
+num2 = int(input("Enter your second number"))
+
+print(f"Addition of two number {num} - {num2} = {num-num2}")
